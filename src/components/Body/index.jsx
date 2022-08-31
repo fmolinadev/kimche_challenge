8412412011fb64f26154a30c6f0ecf98eaae852f
Francisco Molina <fdm.molina@gmail.com>
2022-08-31 17:29:08 -0300

    Update: Obtuve la información de la api para mis estados locales

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_COUNTRIES } from '../../graphql/queryCountries';
 
 const Body = () => {
+  const { data, loading, error } = useQuery(GET_COUNTRIES);
+  const [continent, allContinent] = useState([]);
+  const [language, allLanguage] = useState([]);
+  const [countries, allCountries] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      allContinent(data.continents);
+      allLanguage(data.languages);
+      allCountries(data.countries);
+    }
+  }, [data]);
+
+  console.log('CONTINENTES--->', continent);
+  console.log('LANGUAGES--->', language);
+  console.log('COUNTRIES--->', countries);
   return <div>Body</div>;
 };
 
