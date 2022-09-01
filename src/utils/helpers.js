fea1b878eb29b1515d0c3c34a06bfa601f5e7c09
Francisco Molina <fdm.molina@gmail.com>
2022-09-01 16:32:12 -0300

    Agregue unas funciones con un reducer para armar los grupos de paises

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,4 +2,26 @@ export const goBack = (e) => {
   window.history.go(-1);
 };
 
-export const cleanDecoration = { textDecoration: "none", color: "inherit" };
+export const cleanDecoration = { textDecoration: 'none', color: 'inherit' };
+
+export const filterByContinent = (ref) => {
+  const groups = ref.reduce((groups, item) => {
+    const group = groups[item.continent.name] || [];
+    group.push(item);
+    groups[item.continent.name] = group;
+    return groups;
+  }, {});
+  return Object.entries(groups);
+};
+
+export const filterByLanguage = (ref) => {
+  const groups = ref.reduce((groups, item) => {
+    item.languages.map((len) => {
+      const group = groups[len.name] || [];
+      group.push(item);
+      groups[len.name] = group;
+    });
+    return groups;
+  }, {});
+  return Object.entries(groups);
+};
