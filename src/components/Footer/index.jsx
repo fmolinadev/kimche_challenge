1f5f1e6b4e6243a597040b9252bee88a87bba7e4
Francisco Molina <fdm.molina@gmail.com>
2022-09-01 19:44:31 -0300

    Cree mi footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>Code by: Francisco Molina | www.franciscomolina.com.ar</span>
+    </footer>
+  );
+};
+
+export default Footer;
