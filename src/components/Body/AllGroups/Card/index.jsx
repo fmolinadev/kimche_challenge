fd46ccfacd74f36bc4366e6e38dd13c85fc7c523
Francisco Molina <fdm.molina@gmail.com>
2022-09-01 16:38:43 -0300

    Componente que renderiza la info de cada pais.

---
+++ src/components/Body/AllGroups/Card/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Card = ({ name, capital, emoji, lenguages }) => {
+  return (
+    <article>
+      <header>
+        <div>
+          <h3>Nombre: {name}</h3>
+          <span>{emoji}</span>
+        </div>
+      </header>
+      <div>
+        <h6>Capital: {capital}</h6>
+        {/* <h6>Lenguajes: {lenguages}</h6> */}
+      </div>
+    </article>
+  );
+};
+
+export default Card;
