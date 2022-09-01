e767bc881fdcf8b38540c5cb45d77cb1b091c4a5
Francisco Molina <fdm.molina@gmail.com>
2022-09-01 19:21:06 -0300

    Refactorice mis querys a un solo archivo.

--- src/graphql/queryCountries.js
+++ src/graphql/queryCountries.js
@@ -16,3 +16,32 @@ export const GET_COUNTRIES = gql`
     }
   }
 `;
+
+export const SEARCH_COUNTRY = gql`
+  query getOneCountry($code: ID!) {
+    country(code: $code) {
+      name
+      code
+      native
+      phone
+      continent {
+        name
+      }
+      capital
+      currency
+      languages {
+        name
+      }
+      emoji
+      emojiU
+      states {
+        name
+      }
+    }
+  }
+`;
+
+/* Ejm de query variable:
+{
+  "code": "AR"
+}   */
