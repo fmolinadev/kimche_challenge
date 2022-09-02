905451e071481ac4b801944ecf1ea8b5c00f7817
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 02:43:08 -0300

    Cree los estilos del Header

---
+++ src/styles/HeaderStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px 0px;
+  width: 100%;
+`;
+
+export const Logo = styled.img`
+  max-width: 28px;
+`;
+
+export const Name = styled.span`
+  padding-left: 10px;
+`;
