1266de23fea3f6fec3fb8cc583d077bd02834639
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 02:44:15 -0300

    Update del Footer con estilos

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import * as Styles from '../../styles/FooterStyles';
 const Footer = () => {
   return (
-    <footer>
-      <span>Code by: Francisco Molina | www.franciscomolina.com.ar</span>
-    </footer>
+    <Styles.Footer>
+      <Styles.MiPortfolio>
+        Code by: Francisco D. Molina | www.franciscomolina.com.ar
+      </Styles.MiPortfolio>
+    </Styles.Footer>
   );
 };
 
