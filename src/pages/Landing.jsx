e4461eccc9809a6cd48cf96922aeac873d1c59b0
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 11:18:22 -0300

    Update: Modifique el render de la pagina de aterrizaje a su respectivo componente

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,32 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { cleanDecoration } from '../utils/helpers';
-import * as Style from '../styles/LandingStyles';
+import LandingPage from '../components/Body/LandingPage';
 const Landing = () => {
-  return (
-    <Style.Landing>
-      <Style.LeftSide>
-        <Style.TextContainer>
-          <Style.Brand>Challenge Dev</Style.Brand>
-          <Style.Title>Kimche Countries</Style.Title>
-          <Style.Subtitle>
-            Para conocer más sobre <br />
-            los paises que te interesan.
-          </Style.Subtitle>
-          <Style.Describe>
-            Vos decis el nombre <br />
-            ¡y del resto nos encargamos nosotros!
-          </Style.Describe>
-        </Style.TextContainer>
-        <Style.Button>
-          <Link to="/home" style={cleanDecoration}>
-            Comenzar
-          </Link>
-        </Style.Button>
-      </Style.LeftSide>
-      <Style.RightSide></Style.RightSide>
-    </Style.Landing>
-  );
+  return <LandingPage />;
 };
 
 export default Landing;
