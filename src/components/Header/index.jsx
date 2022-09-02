5a64cde179751371169dc150dc08e7f867316c50
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 02:44:29 -0300

    Update del Header con estilos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Brand from '../../assets/brand.png';
+import * as Style from '../../styles/HeaderStyles';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Style.Header>
+      <Style.Logo src={Brand} alt="logo kimche" aria-label="logo" />
+      <Style.Name>Kimche Challenge</Style.Name>
+    </Style.Header>
+  );
 };
 
 export default Header;
