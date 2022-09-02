e7753fe714924065d930a117c6a5516b1fe26c97
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 14:47:23 -0300

    Update: Render de la información base de cada país

--- src/components/Body/AllGroups/Card/index.jsx
+++ src/components/Body/AllGroups/Card/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-
-const Card = ({ name, capital, emoji, lenguages }) => {
+import * as Styled from '../../../../styles/CardStyles';
+const Card = ({ name, capital, emoji }) => {
   return (
-    <article>
-      <header>
-        <div>
-          <h3>Nombre: {name}</h3>
-          <span>{emoji}</span>
-        </div>
-      </header>
-      <div>
-        <h6>Capital: {capital}</h6>
-      </div>
-    </article>
+    <Styled.Card>
+      <Styled.CardHeader>
+        <Styled.HeaderContainer>
+          <Styled.Alias>{emoji}</Styled.Alias>
+          <Styled.Name>{name}</Styled.Name>
+          <Styled.Capital>
+            Capital: {capital ? capital : 'Sin datos'}
+          </Styled.Capital>
+        </Styled.HeaderContainer>
+        <div></div>
+      </Styled.CardHeader>
+    </Styled.Card>
   );
 };
 
