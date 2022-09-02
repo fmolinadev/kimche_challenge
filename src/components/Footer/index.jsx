2584effa6abcd2113c947cd1f2cddb03bb9b2a96
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 16:43:15 -0300

    Agregue linl a mi portfolio previo al deploy.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { cleanDecoration } from '../../utils/helpers';
 import * as Styles from '../../styles/FooterStyles';
 const Footer = () => {
   return (
     <Styles.Footer>
       <Styles.MiPortfolio>
-        Code by: Francisco D. Molina | www.franciscomolina.com.ar
+        <a
+          href="https://www.franciscomolina.com.ar/"
+          rel="noopener noreferrer"
+          target="_blank"
+          style={cleanDecoration}
+        >
+          Code by: Francisco D. Molina | www.franciscomolina.com.ar
+        </a>
       </Styles.MiPortfolio>
     </Styles.Footer>
   );
