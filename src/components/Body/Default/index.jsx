434bf1e2e2f0e76067b530b2410d3a2e142ffa5a
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 10:31:58 -0300

    Aplique estilos e hice responsive el componente que muestro por default

--- src/components/Body/Default/index.jsx
+++ src/components/Body/Default/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import * as Styled from '../../../styles/DefaultMessageStyles';
+import Brujula from '../../../assets/brujula.gif';
 const Default = () => {
   return (
-    <div>
-      <h2>No se ha ingresado un nombre</h2>
-    </div>
+    <Styled.Default>
+      <Styled.Image
+        src={Brujula}
+        alt="imagen de brujula"
+        aria-label="brujula"
+      />
+      <Styled.Text>No has ingresado un nombre</Styled.Text>
+    </Styled.Default>
   );
 };
 
