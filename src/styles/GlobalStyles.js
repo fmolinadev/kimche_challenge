aea9ccf9f2c39d8ba360340e85d3893f9d8127b1
Francisco Molina <fdm.molina@gmail.com>
2022-09-01 22:30:17 -0300

    Update de los estilos globales

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,16 +1,35 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    font-size: 16px;
-    color: black;
+    color: #1d1d1b;
     max-width: 1400px;
   }
   body {
     margin: auto;
   }
+h1, h2, h3{
+  font-family: 'Roboto', sans-serif;
+  &::selection {
+    background: #f5ae16;
+    color: #1d1d1b;
+  }
+}
+h4, h5,h6, p {
+  font-family: 'Source Sans Pro', sans-serif;
+  &::selection {
+    background: #62c3d2;
+    color: #1d1d1b;
+  }
+} 
+span {
+  &::selection {
+    background: #62c3d2;
+    color: #1d1d1b;
+  }
+}
 `;
 
 export default GlobalStyle;
