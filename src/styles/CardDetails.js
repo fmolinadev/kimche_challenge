47cfa6b044db1b706bb0093190519aa28e4dbbc5
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 16:42:53 -0300

    Update: centre los elementos de la card.

--- src/styles/CardDetails.js
+++ src/styles/CardDetails.js
@@ -65,6 +65,8 @@ export const Detail = styled.p`
 export const DetailContainer = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
+  padding: 2px 0px;
 `;
 
 export const Button = styled.button`
