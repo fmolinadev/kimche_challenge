b7973dc006ea42b4e2bbdeb1f304eadfcf6f5de7
Francisco Molina <fdm.molina@gmail.com>
2022-09-02 00:01:19 -0300

    Update Landing con estilos.

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { cleanDecoration } from "../utils/helpers";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { cleanDecoration } from '../utils/helpers';
+import * as Style from '../styles/LandingStyles';
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing page</h1>
-      <button>
-        <Link to="/home" style={cleanDecoration}>
-          Comenzar
-        </Link>
-      </button>
-    </div>
+    <Style.Landing>
+      <Style.LeftSide>
+        <Style.TextContainer>
+          <Style.Brand>Challenge Dev</Style.Brand>
+          <Style.Title>Kimche Countries</Style.Title>
+          <Style.Subtitle>
+            Para conocer más sobre <br />
+            los paises que te interesan.
+          </Style.Subtitle>
+          <Style.Describe>
+            Vos decis el nombre <br />
+            ¡y del resto nos encargamos nosotros!
+          </Style.Describe>
+        </Style.TextContainer>
+        <Style.Button>
+          <Link to="/home" style={cleanDecoration}>
+            Comenzar
+          </Link>
+        </Style.Button>
+      </Style.LeftSide>
+      <Style.RightSide></Style.RightSide>
+    </Style.Landing>
   );
 };
 
